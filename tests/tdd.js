3f648045a5a0a67025c2c18bed0f67b398a916cb
subinviswanathan <subinv1992@gmail.com>
2020-06-15 16:15:47 +0530

    TDD

---
+++ tests/tdd.js
@@ -0,0 +1,3 @@
+// TDD -Test first then Code is an approach to build software. With this we write tests before
+// writing production code. We start by failing test. Then we make the tests pass.
+// Benefits -- testable source code, full coverage by tests, simpler implemenation.
